b38ef448e215ec03f5a98ed8c6b9105fd179e507
AnatoliyZhartun <as.zhartun@yandex.by>
2022-01-04 13:54:49 +0300

    Task03: add solution. Task01..02: add description of task to Main.java.

--- src/main/java/simplestClassesAndObjects/task02/Main.java
+++ src/main/java/simplestClassesAndObjects/task02/Main.java
@@ -1,5 +1,10 @@
 package simplestClassesAndObjects.task02;
 
+/**
+ * 2. Создйте класс Test2 двумя переменными. Добавьте конструктор с входными параметрами.
+ * Добавьте конструктор, инициализирующий члены класса по умолчанию.
+ * Добавьте set- и get- методы для полей экземпляра класса.
+ */
 public class Main {
     public static void main(String[] args) {
         Test2 test1 = new Test2();
